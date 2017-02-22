cbed12231c807c543cab1b0e973465bf0ec21fb1
ru_shalm <ru_shalm@hazimu.com>
2017-02-23 02:15:55 +0900

    check login when edit tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 class TagsController < ApplicationController
+  before_action :require_login!, only: [:edit, :update]
   before_action :set_tag, only: [:show, :popular, :edit, :update]
   before_action :set_page, only: [:show, :popular]
 
