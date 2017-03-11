dbc579b7e5f067c1755c406c464fb7390dc17f69
Takafumi ONAKA <takafumi.onaka@gmail.com>
2017-03-11 23:19:05 +0900

    Make ArticleBuilder#build a little better
    Extract `build_revision`, `build_article` methods.
    These are factory methods that assign attributes.

--- app/builders/article_builder.rb
+++ app/builders/article_builder.rb
@@ -11,9 +11,31 @@ class ArticleBuilder
   attr_reader :errors
 
   def build(params)
-    @revision.user_id = params.delete(:user_id)
-    @article.user_id ||= @revision.user_id
+    build_revision(params)
+    build_article(params)
 
+    ActiveRecord::Base.transaction do
+      @article.save!
+
+      @revision.article_id = @article.id
+      @revision.save!
+
+      @article.newest_revision_id = @revision.id
+      @article.save!
+
+      @article.tags_text = @revision.tags_text
+    end
+
+    notify(@article) if !@published && @article.public_item?
+
+    true
+  rescue ActiveRecord::RecordInvalid => e
+    @errors = e.record.errors
+    false
+  end
+
+  def build_revision(params)
+    @revision.user_id = params.delete(:user_id)
     @revision.title = params.delete(:title)
     @revision.body = params.delete(:body)
     @revision.note = params.delete(:note).to_s
@@ -32,28 +54,13 @@ class ArticleBuilder
       Rails.logger.debug @error.inspect
       raise Errors::BadRequest
     end
+  end
 
-    ActiveRecord::Base.transaction do
-      @revision.save!
-
-      @article.user_id ||= @revision.user_id
-      @article.title = @revision.title
-      @article.newest_revision_id = @revision.id
-      @article.publish_type = params[:publish_type]
-      @article.published_at ||= Time.zone.now if @revision.published?
-      @article.save!
-
-      @article.tags_text = @revision.tags_text
-      @revision.article_id = @article.id
-      @revision.save!
-    end
-
-    notify(@article) if !@published && @article.public_item?
-
-    true
-  rescue ActiveRecord::RecordInvalid => e
-    @errors = e.record.errors
-    false
+  def build_article(params)
+    @article.user_id ||= @revision.user_id
+    @article.title = @revision.title
+    @article.publish_type = params[:publish_type]
+    @article.published_at ||= Time.zone.now if @revision.published?
   end
 
   def notify(article)
