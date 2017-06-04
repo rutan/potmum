5f6d5007663cc5df7ea742cad09de651a8fc938e
ru_shalm <ru_shalm@hazimu.com>
2017-06-04 17:10:02 +0900

    fetch user by id, name or myself

--- app/apis/graph/queries/root_query.rb
+++ app/apis/graph/queries/root_query.rb
@@ -9,18 +9,7 @@ module Graph
       field :article, field: Graph::Queries::Articles::Show
       field :search, field: Graph::Queries::Articles::Search
 
-      field :user do
-        type -> { Types::User }
-        description 'Get a user'
-
-        argument :id, types.Int
-
-        resolve -> (_object, args, context) do
-          ::User.find(args[:id]).tap do |user|
-            Pundit.authorize(context[:access_token], user, :show?)
-          end.decorate
-        end
-      end
+      field :user, field: Graph::Queries::Users::Show
     end
   end
 end
