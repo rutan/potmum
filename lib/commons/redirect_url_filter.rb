3060c06d154ceab635325990cc5680526f2b41d1
ru_shalm <ru_shalm@hazimu.com>
2017-11-26 17:50:49 +0900

    refactor RedirectURLFilter#call

--- lib/commons/redirect_url_filter.rb
+++ lib/commons/redirect_url_filter.rb
@@ -7,19 +7,35 @@ class RedirectURLFilter < HTML::Pipeline::Filter
     doc.search('a').each do |node|
       url = node.attr('href')
       next unless url
-
-      next if url.start_with?('#') # OK: #hoge
-      next if url.match?(%r{\A/[^/]}) # OK: /hoge
-      if GlobalSetting.root_url.present?
-        next if url.start_with?("#{GlobalSetting.root_url}/") # OK: http://podmum-url/hoge
-        next if url.start_with?("//#{GlobalSetting.root_url.sub(/\Ahttps?:/, '')}/") # OK: //podmum-url/hoge
-      end
-      if GlobalSetting.attachment_file_s3?
-        next if url.start_with?(GlobalSetting.attachment_file_s3_host) # OK
-      end
+      next if local_link?(url)
+      next if site_link?(url)
+      next if attachment_file_link?(url)
 
       node.set_attribute('href', "/redirect?url=#{CGI.escape(url)}")
     end
     doc
+  end
+
+  private
+
+  def local_link?(url)
+    return true if url.start_with?('#') # ex) #hoge
+    return true if url.match?(%r{\A/[^/]}) # ex) /hoge
+
+    false
+  end
+
+  def site_link?(url)
+    return false unless GlobalSetting.root_url.present?
+
+    return true if url.start_with?("#{GlobalSetting.root_url}/") # ex) http://podmum-url/hoge
+    return true if url.start_with?("//#{GlobalSetting.root_url.sub(/\Ahttps?:/, '')}/") # ex) //podmum-url/hoge
+
+    false
+  end
+
+  def attachment_file_link?(url)
+    return false unless GlobalSetting.attachment_file_s3?
+    url.start_with?(GlobalSetting.attachment_file_s3_host)
   end
 end
