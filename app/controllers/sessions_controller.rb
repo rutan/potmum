9143db2d5af3530cd9d67df685529fe4f49f0174
ru_shalm <ru_shalm@hazimu.com>
2017-11-26 17:44:35 +0900

    refactor SessionsController#callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,26 +10,14 @@ class SessionsController < ApplicationController
 
     user = User.find_by_auth(auth)
 
-    if current_user
-      # 既存アカウントとのヒモ付処理へ
-      session[:auth] = {
-        provider: auth[:provider],
-        uid: auth[:uid],
-        user_id: user.try(:id)
-      }
-      redirect_to new_users_authentication_path
-    elsif user
-      # ログインへ
+    case
+    when current_user
+      redirect_to_connect_exist_user(user, auth)
+    when user
       session[:user_id] = user.id
       redirect_to '/'
     else
-      # アカウント新規登録画面へ
-      session[:auth] = {
-        provider: auth[:provider],
-        uid: auth[:uid],
-        info: { nickname: auth[:info][:nickname] }
-      }
-      redirect_to register_path
+      redirect_to_register(auth)
     end
   end
 
@@ -40,5 +28,27 @@ class SessionsController < ApplicationController
   def destroy
     session.clear
     redirect_to '/'
+  end
+
+  private
+
+  # 既存アカウントとのヒモ付処理へ
+  def redirect_to_connect_exist_user(user, auth)
+    session[:auth] = {
+      provider: auth[:provider],
+      uid: auth[:uid],
+      user_id: user.try(:id)
+    }
+    redirect_to new_users_authentication_path
+  end
+
+  # アカウント新規登録画面へ
+  def redirect_to_register(auth)
+    session[:auth] = {
+      provider: auth[:provider],
+      uid: auth[:uid],
+      info: { nickname: auth[:info][:nickname] }
+    }
+    redirect_to register_path
   end
 end
