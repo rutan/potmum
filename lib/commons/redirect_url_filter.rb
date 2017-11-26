2b505cdf12dea26f8c29f5996803ee757822d1fa
ru_shalm <ru_shalm@hazimu.com>
2017-11-26 16:42:41 +0900

    fix rubocop `.zero?` instead `== 0`

--- lib/commons/redirect_url_filter.rb
+++ lib/commons/redirect_url_filter.rb
@@ -7,14 +7,14 @@ class RedirectURLFilter < HTML::Pipeline::Filter
       url = node.attr('href')
       next unless url
 
-      next if url.index('#') == 0 # OK: #hoge
+      next if url.start_with?('#') # OK: #hoge
       next if url =~ /\A\/[^\/]/ # OK: /hoge
       if GlobalSetting.root_url.present?
-        next if url.index("#{GlobalSetting.root_url}/") == 0 # OK: http://podmum-url/hoge
-        next if url.index("//#{GlobalSetting.root_url.sub(/\Ahttps?:/, '')}/") == 0 # OK: //podmum-url/hoge
+        next if url.start_with?("#{GlobalSetting.root_url}/") # OK: http://podmum-url/hoge
+        next if url.start_with?("//#{GlobalSetting.root_url.sub(/\Ahttps?:/, '')}/") # OK: //podmum-url/hoge
       end
       if GlobalSetting.attachment_file_s3?
-        next if url.index(GlobalSetting.attachment_file_s3_host) == 0 # OK
+        next if url.start_with?(GlobalSetting.attachment_file_s3_host) # OK
       end
 
       node.set_attribute('href', "/redirect?url=#{CGI.escape(url)}")
